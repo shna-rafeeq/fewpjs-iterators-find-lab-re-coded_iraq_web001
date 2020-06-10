8fa236f568cf22e4b8eaf39c406cbcc9b7f54bf2
shna rafeeq <shnar.majeed@gmail.com>
2020-06-10 11:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
-const sadReality = record.map( yObj => Object.assign({}, yObj, { result: "N/A" } ) )
 
-function superbowlWin(sadReality){
- let newarr = sadReality.find((item)=>{
+function superbowlWin(arr){
+ let newarr = arr.find((item)=>{
    
   return item.result === "W"
  
